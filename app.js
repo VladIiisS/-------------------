424d7c532530bfa1e26e159a12032126f44775de
VladIiisS <105129834+VladIiisS@users.noreply.github.com>
2022-05-27 21:13:54 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,110 +1,52 @@
+ import AppInfo from '../app-info/app-info';
+ import SearchPanel from '../search-panel/search-panel';
+ import AppFilter from '../app-filter/app-filter';
+ import EmployeesList from '../employees-list/employees-list';
+ import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+ import './app.css';
+
+ function App() {
+
+    const data = [
+      {name: 'Stiv', salary: 456, increase: false },
+        {name: 'Maks', salary: 876, increase: true},
+         {name: 'Ivan', salary: 1214, increase: false},
+    ];
+   return (
+     <div className="app">
+         <AppInfo />
+
+         <div className="search-panel">
+             <SearchPanel/>
+             <AppFilter/>
+         </div>
+        
+         <EmployeesList data = {data}/>
+         <EmployeesAddForm/>
+     </div>
+   );
+ }
+
+ export default App;
 
 
-//const div = document.querySelector('div');
-//const titels = document.querySelector('h1');
+// import './App.css';
 
+//   function WhoAmI ({name, surname, link}) {
+//       return (
+//          <div> 
+//             <h1> My name is { name}, surname - {surname}</h1>
+//                <a href={props.link}>My profile</a>
 
-//const link = div.querySelector('.link');
-//console.log(link.parentElement);
-//console.log(link.closest('.content')); 
-
-//console.log(div.classList);
-
-
-
-   // const div = document.querySelector('div');
-   // const titels = document.querySelector('h1');
-   // const link = div.querySelector('.link');
-   /// console.log(link.parentElement);
- //   console.log(link.closest('.content')); 
-  //  console.log(div.classList);
-  //  <><div>First div</div><script>
-  //  let divs = document.querySelectorAll('div');
-  //  alert(divs.length);
-//</script><div>Second div</div><script>
- //       alert(divs.length);
- //   </script></>;
-
-
-
- //const title = document.querySelector('h1');
- //title.innerHTML = "<span> Hi</span>";
-// title.insertAdjacentHTML("beforebegin", '<h2>title</h2>');
-
-//const span = document.createElement('span');
-//span.textContent = 'hello my friends';
-//span.classList.add('myClass');
-//console.log(span);
-//title.appendChild(span);
-
-//const fragment = document.createDocumentFragment();
-//console.log(fragment);
-//const colors = ["black", "orange", "brown"];
-//colors.forEach((color) => {
-   // const item = document.createElement("div");
-   /// item.classList.add(`bg-${color}`);
-   // item.style.background = color;
-    //item.textContent = color;
-   // fragment.appendChild(item);
-
-//});
-
-//document.body.appendChild(fragment);
-
-//////////////////////////////////////////////////
-
-const btn = document.querySelector('button');
-const link = document.querySelector('a');
-//btn.onclick = function() {
-  //console.log('click');
-//};
-
-//btn.addEventListener("click", function(e) {
- // console.log(e);
-//});
-
-//btn.addEventListener("click", function() {
- // console.log('click 2')
-//});
-
-//btn.addEventListener("click", (e) => {
-//  console.log(this);
-//console.log(e);
-//});
-
-//function clickhander (e) {
- // e.preventDefault();
- // console.log(this);
-//console.log('click');
-//}
-
-//link.removeEventListener("click", clickhander); 
-//link.removeEventListener("click", clickhander); 
-
-const container = document.querySelector(".container");
-
-btn.addEventListener('click',(e) => {
-const div = document.createElement('div');
-const nastedBtn = document.createElement('button');
-div.textContent = Math.random();
-nastedBtn.textContent = 'button'
-div.appendChild(nastedBtn);
-container.appendChild(div);
-});
-
-container.addEventListener('click', e => {
-console.log(e.target);
-if (e.target.tagName === "BUTTON") {
-  console.log('button clicked');
-}
-});
-
-
-
-
-
-
-
-
-
-
+//          </div>
+//       )
+//   }
+//    function App () {
+//       return (
+//          <div className="App">
+//             <WhoAmI name="John" surname="Smith" link="fecebook.com"/>
+//              <WhoAmI name="Alex" surname="Spilberg" link="instagram.com"/>
+//          </div>
+//       );
+//    }
